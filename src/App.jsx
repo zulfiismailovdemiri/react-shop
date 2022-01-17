e221636efb13c1ad73ea33dbc56bac5c85e7cdda
Zulfi Ismailov-Demiri <id@werkkasten.com>
2022-01-17 09:01:04 +0100

    init project on github

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <header className="w-full bg-gray-600 flex items-center px-4">
+        <div className="brand font-bold text-white text-3xl">Shop</div>
+        <ul className="ml-auto flex py-12 text-white font-bold">
+          <li className="ml-4">Cart</li>
+          <li className="ml-4">Sign In</li>
+        </ul>
+      </header>
+      <main className="py-12 px-4 w-full">
+        <div className="product-list flex items-center">
+          {data.products.map((product) => (
+            <div
+              key={product._id}
+              className="product-item mr-4 p-4 bg-gray-100 rounded-md shadow"
+            >
+              <div className="product-item-content flex flex-col">
+                <a href={`product/${product._id}`}>
+                  <img src={product.image} alt="" />
+                </a>
+                <div className="font-bold mb-4 text-2xl">
+                  <a href={`product/${product._id}`}>{product.name}</a>
+                </div>
+                <div className="mb-4">{product.description}</div>
+                <div className="mb-4 font-bold text-2xl">{product.price} €</div>
+                <div>Ratings: {product.ratings}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = {
+  products: [
+    {
+      _id: 1,
+      name: 'Some product Name',
+      description: 'Some Product Description',
+      image: 'someImage.jpg',
+      price: 120,
+      ratings: 4,
+    },
+    {
+      _id: 2,
+      name: 'Some New product',
+      description: 'grate product description',
+      image: 'someImage.jpg',
+      price: 100,
+      ratings: 5,
+    },
+  ],
+};
+
+export default data;
