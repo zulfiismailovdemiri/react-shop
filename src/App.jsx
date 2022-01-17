9ae0e6c3730af568e622dc31f666b9fe4395d1ca
Zulfi Ismailov-Demiri <id@werkkasten.com>
2022-01-17 11:16:55 +0100

    styling action scope on productscreen

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <header className="w-full bg-gray-600 flex items-center px-4">
-          <div className="brand font-bold text-white text-3xl">Shop</div>
+        <header className="w-full bg-gray-600 flex items-center px-4 mb-4">
+          <div className="brand font-bold text-white text-3xl">
+            <a href="/">Shop</a>
+          </div>
           <ul className="ml-auto flex py-12 text-white font-bold">
             <li className="ml-4">Cart</li>
             <li className="ml-4">Sign In</li>
           </ul>
         </header>
-        <main className="py-12 px-4 w-full">
+        <main className="px-4 w-full">
           <Routes>
             <Route path="/product/:id" element={<ProductScreen />}></Route>
             <Route path="/" element={<HomeScreen />} exact></Route>
--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,5 +1,53 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+
+import data from '../data';
 
 export default function ProductScreen() {
-  return <div>ProductScreen</div>;
+  const { id } = useParams();
+  const product = data.products.find((x) => x._id === Number(id));
+  if (!product) {
+    return <div>No Product Found</div>;
+  }
+  return (
+    <div>
+      <div>
+        <Link to="/" className="text-blue-400">
+          Go Back
+        </Link>
+      </div>
+      <div className="flex w-full justify-between">
+        <div className="w-1/2">Image</div>
+        <div className="flex flex-col lg:flex-row justify-between w-1/2">
+          <div className="mb-12 w-full lg:w-2/3">
+            <h1 className="font-bold text-3xl mb-2">{product.name}</h1>
+            <div className="mb-8">
+              <Rating rating={product.rating} numReviews={product.numReviews} />
+            </div>
+            <div>{product.description}</div>
+          </div>
+          <div className="w-full lg:w-1/3 p-4 border border-gray-200 rounded">
+            <div className="flex flex-col mb-4">
+              <div>
+                <div className="text-sm font-bold">Price</div>
+                <div className="font-bold text-2xl">{product.price} €</div>
+                <div>
+                  Status:
+                  {product.countInStock > 0 ? (
+                    <span> Available</span>
+                  ) : (
+                    <span> Not Available</span>
+                  )}
+                </div>
+              </div>
+            </div>
+            <button type="submit" className="w-full p-4 bg-yellow-400 rounded">
+              Buy
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
